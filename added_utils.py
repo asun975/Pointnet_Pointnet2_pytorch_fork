c569fb7da677e1cc52a181e7805c7bea54472d26
asun975 <65915716+asun975@users.noreply.github.com>
2024-07-04 11:37:47 -0400

    Update added_utils.py

--- added_utils.py
+++ added_utils.py
@@ -1,4 +1,6 @@
-# zero to mastery
+import matplotlib.pyplot as plt
+
+# zero to mastery pytorch course by mdbourke (github)
 def print_train_time(start, end, device=None):
     """Prints difference between start and end time.
 
@@ -13,3 +15,26 @@ def print_train_time(start, end, device=None):
     total_time = end - start
     print(f"\nTrain time on {device}: {total_time:.3f} seconds")
     return total_time
+
+def plot_loss_curves(results):
+    acc = results["train_acc"]
+    test_accuracy = results["test_acc"]
+    class_accuracy = results["class_acc"]
+    epochs = range(len(results["train_acc"]))
+
+    plt.figure(figsize=(15, 7))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, acc, label="train_accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy %")
+    plt.title("Training Accuracy")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.plot(epochs, class_accuracy, label="class_accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy %")
+    plt.title("Validation Accuracy")
+    plt.legend()
+    plt.show()
+    
